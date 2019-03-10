4dcc8bf9d38188018d9bfc6b73bf519bc1d05465
bialx <coulaud.jeremie@laposte.net>
2019-03-10 20:59:53 +0100

    not much

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import lib.opening.py
-import lib.menu.py
-import lib.get_url.py
+import lib.opening as opening
+import lib.menu as menu
+import lib.get_url as get_url
 
 
 
 
 
 if __name__ == '__main__':
-    opening_partial, opening_full = build_dict()
-    with open("output/opening.txt", "w") as f:
-        for key, item in opening_partial.items():
-            nbr_win, nbr_match = item
-            f.write(key + "-- nombre win: " + str(nbr_win) + "--nombre match: " + str(nbr_match))
-            f.write("\n")
+    opening_partial, opening_full = opening.build_dict()
+    # with open("output/opening.txt", "w") as f:
+    #     for key, item in opening_partial.items():
+    #         nbr_win, nbr_match = item
+    #         f.write(key + "-- nombre win: " + str(nbr_win) + "--nombre match: " + str(nbr_match))
+    #         f.write("\n")
+    opening.display_info_openings(opening_partial)
